494d3b9d4542fe5c8059688222261d72c0b2c0fa
zhu <zhu@zhu.moe>
2024-07-26 14:53:47 +0800

    更新大麦和纷玩岛的刷票流程（利用预售填过信息会自动点选，要比点来点去快一些）

--- core/damai.py
+++ core/damai.py
@@ -5,7 +5,7 @@ from logger.logger import logger
 class DaimaiBot(TicketBot):
     """
     大麦Bot
-    绝大部分依靠resource id、上下层关系来定位
+    绝大部分依靠resource id、上下层关系来定位（观察下后续，要是resid老改的话，可以考虑几个都改成坐标）
 
     """
 
@@ -30,33 +30,29 @@ class DaimaiBot(TicketBot):
         V1: 循环点击两个门票（好像是没作用，文件最下面留了个备份）
         V2：循环点击两个场次（有用，但是需要sleep至少4-5s，不知道是不是个例，待验证）
         V3：最原始的法子：出去再进来
+        V4：利用APP预填信息，有票的话会自动给选上来刷票（反复进出页面，判断magic word）
 
         """
         ticket_num = len(self.config.buyer.info)
         total_price = ticket_num * self.config.ticket.target_price
         while True:
-            # 点击场次
-            self.sel_children(self.damai_perform_flow, ticket_tier, "android.view.ViewGroup").click()
-            # 点击票价
-            self.sel_children(self.damai_price_flow, target_tier, "android.widget.FrameLayout").click()
-            time.sleep(0.05)
+            # 等待页面加载、给150ms自动选择时间
+            self.sel_by_text("¥").wait(3)
+            time.sleep(0.15)
+            # 判断magic word
             if self.sel_by_text(magic_word).exists:
-                if ticket_num > 1:
-                    if not self.sel_by_text(total_price).exists:
-                        logger.info("余票不足{0}张，跳过".format(ticket_num))
-                        self.dev.press("back")
-                        self.sel_by_resid("cn.damai:id/trade_project_detail_purchase_status_bar_container_fl").click()
-                        continue
-                logger.info("刷出票价：{0}档，尝试进入下单页面".format(target_tier))
-                return True
-            else:
-                self.dev.press("back")
-                self.sel_by_resid("cn.damai:id/trade_project_detail_purchase_status_bar_container_fl").click()
+                if self.sel_by_text(total_price).exists:
+                    logger.info("刷出票价：{0}档，尝试进入下单页面".format(target_tier))
+                    return True
+                else:
+                    logger.info("余票不足{0}张，跳过".format(ticket_num))
+            self.dev.press("back")
+            self.sel_by_resid("cn.damai:id/trade_project_detail_purchase_status_bar_container_fl").click()
 
     def order_workflow(self):
         """
         下单流程的函数
-        主要功能：点击确定→下单→alter check（需再循环一次return False，下单完成、出错etc return True）
+        主要功能：点击确定→下单→alter check，需再循环一次return False，下单完成（或未知情况）return True）
 
         """
         # 点击确定
@@ -68,6 +64,7 @@ class DaimaiBot(TicketBot):
             # 点击提交订单
             self.sel_by_text("实名观演人").wait(10)
             self.sel_by_resid("cn.damai:id/bottom_layout").child(index=7, className="android.widget.FrameLayout").click()
+            # 弹窗检测
             hint = self.alert_check(["继续尝试", "我知道了"], 10)
             if hint == "继续尝试":
                 logger.info("出现'{0}'弹窗，继续运行...".format(hint))
@@ -79,7 +76,6 @@ class DaimaiBot(TicketBot):
                 self.screenshot()
                 self.sel_by_text(hint).click()
                 return False
-            # 还少支付界面的提示
             else:
                 logger.info("未知情况（可能抢到了，可能出错了），请查看截图")
                 self.screenshot()
@@ -88,34 +84,35 @@ class DaimaiBot(TicketBot):
     def damai_presale(self):
         """
         大麦预售流程
-        0、预先选好场次、票档、观演人
-        1、进入预售页面
-        2、运行脚本
+        1、预先选好场次、票档、观演人（重要！）
+        2、进入预售页面
+        3、运行脚本
         
         """
-        logger.info("=== 大麦预售流程 ===")
+        logger.info("进入大麦预售流程...")
         # 定时运行
         if self.trigger(self.config.scheduler.trigger):
+            logger.info("到达设定时间，开始抢票")
             # 点击立即预定
             self.sel_by_resid("cn.damai:id/trade_project_detail_purchase_status_bar_container_fl").click()
             while True:
-                # 开始下单
+                # 运行下单流程
                 if self.order_workflow():
                     return
 
     def damai_encore(self):
         """
-        大麦回流票流程V2（预售时填好人数的话，不用再选票数和观演人的，去掉相关流程）
-        1、预售时选好观演人
+        大麦回流票流程V2
+        1、需在预售时选好了场次、票档、观演人（重要！）
         2、进入选票（刷票）页面
         3、运行脚本
         
         """
-        logger.info("=== 大麦回流票流程 ===")
+        logger.info("进入大麦回流票流程...")
         while True:
             # 查库存
             if self.ticket_check(self.config.ticket.ticket_tier, self.config.ticket.target_tier, self.config.ticket.coop_tier, "价格明细"):
-                # 开始下单
+                # 开始下单流程
                 if self.order_workflow():
                     return
 
@@ -124,12 +121,18 @@ class DaimaiBot(TicketBot):
         self.sel_by_text("观演人").click()
         for name, info in self.config.buyer.info.items():
             logger.info("开始添加观演人，姓名：{0}，身份证：{1}".format(name, info[0]))
+            # 点击添加
             self.sel_by_text("添加新观演人").click()
+            # 填写姓名
             self.sel_by_text("请填写观演人姓名").click()
             self.sel_by_text("请填写观演人姓名").send_keys(name)
+            # 填写身份证号
             self.sel_by_text("请填写证件号码").click()
             self.sel_by_text("请填写证件号码").send_keys(info[0])
+            # 关闭输入法窗口
             self.dev.press("back")
+            # 勾选同意
             self.sel_by_index(0, "android.widget.CheckBox").click()
+            # 点击确定
             self.sel_by_text("确定").click()
             time.sleep(1)
--- core/livelab.py
+++ core/livelab.py
@@ -1,5 +1,4 @@
 import time
-from itertools import cycle
 from core.ticketbot import TicketBot
 from logger.logger import logger
 
@@ -26,42 +25,39 @@ class LivelabBot(TicketBot):
                 return None
 
     def ticket_check(self, ticket_tier, target_tier, coop_tier, magic_word):
-        """刷票程序（不断点击两个票价，出现break_word后break，返回True"""
+        """
+        刷票程序V2
+        V1：不断点击两个票价，出现break_word后break，返回True
+        V2：逻辑同大麦，不断进出页面来刷票（V1需要两次点击才能刷一次，大概需要2-3s）
+
+        """
         ticket_num = len(self.config.buyer.info)
         total_price = "￥{0}.00".format(ticket_num * self.config.ticket.target_price)
-        # 点击场次
-        self.sel_by_desc(ticket_tier).click()
-        # 上滑
-        self.dev.swipe_ext("up", scale=0.9)
-        # 循环池子
-        tier_pool = cycle([target_tier, coop_tier])
-        # 开始循环
         while True:
-            next_tier = next(tier_pool)
-            # 点击对应的票价
-            self.sel_by_desc(next_tier).click()
-            # 如果tier == target_tier，则判断是否出现magic word（可以触发点击确定的提示）
-            if next_tier == target_tier:
-                time.sleep(0.05)
-                if self.sel_by_desc(magic_word).exists:
-                    if ticket_num > 1:
-                        if not self.sel_by_desc(total_price).exists:
-                            logger.info("余票不足{0}张，跳过".format(ticket_num))
-                            continue
+            # 等待页面加载、给150ms自动选择时间
+            self.sel_by_desc("请选择票品").wait(3)
+            time.sleep(0.15)
+            # 判断magic word
+            if self.sel_by_desc(magic_word).exists:
+                if self.sel_by_desc(total_price).exists:
                     logger.info("刷出票价：{0}，尝试进入下单页面".format(target_tier))
                     return True
+                else:
+                    logger.info("余票不足{0}张，跳过".format(ticket_num))
+            self.dev.press("back")
+            self.sel_by_desc("立即购买").click()
 
     def order_workflow(self):
         """
         下单流程的函数
-        主要功能：点击确定→下单→alter check（需再循环一次return False，下单完成、出错etc return True）
+        主要功能：点击确定→下单→alter check，需再循环一次return False，下单完成（或未知情况）return True）
 
         """
         # 点击确认
         self.sel_by_desc("确认").click()
         # 点击提交订单
         self.sel_by_desc("提交订单").click()
-        # 查弹窗
+        # 弹窗检测
         hint = self.alert_check(["请求人数多", "数量不足", "订单中包含已购买"], 10)
         if hint == "请求人数多":
             logger.info("出现'{0}'弹窗，继续运行...".format(hint))
@@ -87,25 +83,26 @@ class LivelabBot(TicketBot):
     def livelab_presale(self):
         """
         纷玩岛预售流程
-        1、预先选好场次、票档、观演人
+        1、预先选好场次、票档、观演人（重要！）
         2、进入预售页面
         3、运行脚本
 
         """
-        logger.info("=== 纷玩岛预售流程 ===")
+        logger.info("进入纷玩岛预售流程...")
         # 定时启动
         if self.trigger(self.config.scheduler.trigger):
+            logger.info("到达设定时间，开始抢票")
             # 点击立即购买
             self.sel_by_desc("立即购买").click()
             while True:
-                # 下单流程
+                # 运行下单流程
                 if self.order_workflow():
                     return
 
     def livelab_encore(self):
         """
-        纷玩岛回流票流程V2（先去掉V1的选票数和选观演人的步骤，八成会记忆，不行再改回来）
-        1、预售时选好观演人
+        纷玩岛回流票流程V2
+        1、需在预售时选好了场次、票档、观演人（重要！）
         2、进入选票（刷票）页面
         3、运行脚本
 
@@ -114,7 +111,7 @@ class LivelabBot(TicketBot):
         while True:
             # 查余票
             if self.ticket_check(self.config.ticket.ticket_tier, self.config.ticket.target_tier, self.config.ticket.coop_tier, "购买数量"):
-                # 下单流程
+                # 开始下单流程
                 if self.order_workflow():
                     return
 
